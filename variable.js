969452a97ebb935818599b7ba07327756649b93d
samruddhi-t0623 <samruddhi2366@gmail.com>
2026-06-05 22:28:42 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,11 @@
+//var let const -diff type
+//redecaration reinitialization hosting
+//var a="hello";
+//var a=12.34;
+//console.log(a);
+//var a="hello";
+//a=20;
+//console.log(a);
+//var a="hello";
+//a=20;
+//console.log(a);
